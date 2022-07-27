e478310091ea000f8a6810584f8fbebeccd90d07
Anton Polyakov <anton.polyakov@datarobot.com>
2022-07-27 10:26:43 -0700

    v7

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "<html><body><center><font size=\"32\">hello 3</font></center></body></html>\n")
+	fmt.Fprintf(w, "<html><body><center><font size=\"32\">hello 4</font></center></body></html>\n")
 }
 
 func main() {
